7e7d3052a9aa0f1b71cff6f3a255d0484dbc1289
2128746 <89657547+2128746@users.noreply.github.com>
2022-12-16 22:48:12 -0500

    Changed Installer

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd
 # Clone the repository
-git clone https://github.com/your-repo-url script
+git clone https://github.com/2128746/SplashScreen
 
 # Ask the user if they want an analog or digital clock
 echo "Do you want an analog or digital clock? (a/d)"
@@ -9,9 +9,9 @@ read clock_type
 
 # Set the clock type in script.bash
 if [ "$clock_type" = "a" ]; then
-  sed -i '6s/.*/isDigital=false/' script.bash
+  sed -i '6s/.*/isDigital=false/' ./SplashScreen/script.bash
 else
-  sed -i '6s/.*/analog=true/' script.bash
+  sed -i '6s/.*/analog=true/' ./SplashScreen/script.bash
 fi
 
 # Ask the user if they want to use their IP or a specific city for the weather
@@ -20,7 +20,7 @@ read weather_type
 
 # Set the weather type in script.bash
 if [ "$weather_type" = "ip" ]; then
-  sed -i '7s/.*/location=/' script.bash
+  sed -i '7s/.*/location=/' ./SplashScreen/script.bash
 else
   echo "Enter the city you want to use for the weather: "
   read city
